8681c38b8db4c2ff1ce0954500cea9991d0cf990
Lei Wei <idea.lew@gmail.com>
2021-07-20 16:30:53 +0200

    add 4x4

--- Assets/Scripts/Parameters.cs
+++ Assets/Scripts/Parameters.cs
@@ -11,7 +11,7 @@
     #region PlayGround
     public static int DimZ = 4;
     public static int DimX = 1;
-    public static int MaxLayer = 3;
+    public static int MaxLayer = 4;
     
     public static readonly int MinDim = 0; // this is only for test (random generation), should be smaller than the min Dim value
 
