84968654266897fcb91559ee41d7f233385c9b72
Lei Wei <idea.lew@gmail.com>
2021-07-26 15:15:27 +0200

    fix indexFull!

--- Assets/Scripts/Bay2DAgent.cs
+++ Assets/Scripts/Bay2DAgent.cs
@@ -360,7 +360,7 @@ public class Bay {
     }
 
     public bool IndexFull(int z) {
-        return layout[z].Count >= DimZ;
+        return layout[z].Count >= MaxTier;
     }
 
     public bool IndexEmpty(int z) {
